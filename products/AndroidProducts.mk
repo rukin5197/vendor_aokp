81b73cb926e21bf60b3612de3d50ecf272b34792
rohan <rohanmathur34@gmail.com>
2012-10-28 21:56:33 -0400

    devices: add in hercules

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -4,6 +4,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/d2vzw.mk \
     $(LOCAL_DIR)/evita.mk \
     $(LOCAL_DIR)/grouper.mk \
+    $(LOCAL_DIR)/hercules.mk \
     $(LOCAL_DIR)/maguro.mk \
     $(LOCAL_DIR)/quincyatt.mk \
     $(LOCAL_DIR)/toro.mk \
